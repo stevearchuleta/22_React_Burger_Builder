14ec125e321a42521e1a8cf81a87865de22a98d5
Steve Archuleta <stevearchuleta@me.com>
2019-12-09 10:03:52 -0800

    calculate ingredient sum dynamically; reduce transformedIngredients array; prompt user to add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,18 +5,27 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = ( props ) => {
   // CREATE AN APPROPRIATE AMOUNT OF BURGER INGREDIENT COMPONENTS: call Object.keys method to convert the keys of a given object (state.ingredients properties in BurgerBuilder.js) into an array - an array of keys (which happen to be strings) -- then, I must first map throught the array of strings to transform the specific string value of an ingredient into an array that has as many elements as I have ingredients for that given ingredient (e.g.: 2 slices of cheese); then, I return JSX in-the-creation-of a new array (hence my use of the spread operator) with an array length of the given ingredient and map through this new array (the first element value is unimportant so I use an underscore as its argument, but the second element i is my index count) and the BurgerIngredient component will now include the creation of a unique key for each ingredient.   
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map((ingKey) => {
       return [...Array(props.ingredients[ingKey])].map((_, i) => {
-       return <BurgerIngredient key={ingKey + i} type={ingKey} />
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />
       });
-    });
+    } )
+    // reduce automatically takes two arguments: previous value, current value.
+    .reduce( (arr, el) => {
+      return arr.concat(el)
+    }, []);
+    // console.log('[Burger.js]', transformedIngredients);
+  if(transformedIngredients.length === 0 ) {
+    transformedIngredients = <p>Please add ingredients.</p>
+  }  
+ 
   return(
-   <div className={classes.Burger}>
+  <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
       {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
-   </div>
+  </div>
   );
 };
 
