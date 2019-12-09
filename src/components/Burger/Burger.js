1d8e5fd429294945f6d024c6ea6f52c177db6051
Steve Archuleta <stevearchuleta@me.com>
2019-12-09 09:26:31 -0800

    initialize state in BurgerBuilder.js; pass state to ingredient prop in BurgerBuilder.js; in Burger.js output ingredients dynamically by transforming the ingredients object of key-value pairs into an array of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,20 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = ( props ) => {
+  // CREATE AN APPROPRIATE AMOUNT OF BURGER INGREDIENT COMPONENTS: call Object.keys method to convert the keys of a given object (state.ingredients properties in BurgerBuilder.js) into an array - an array of keys (which happen to be strings) -- then, I must first map throught the array of strings to transform the specific string value of an ingredient into an array that has as many elements as I have ingredients for that given ingredient (e.g.: 2 slices of cheese); then, I return JSX in-the-creation-of a new array (hence my use of the spread operator) with an array length of the given ingredient and map through this new array (the first element value is unimportant so I use an underscore as its argument, but the second element i is my index count) and the BurgerIngredient component will now include the creation of a unique key for each ingredient.   
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((ingKey) => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+       return <BurgerIngredient key={ingKey + i} type={ingKey} />
+      });
+    });
   return(
    <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
    </div>
-  )
+  );
 };
 
 export default burger;
