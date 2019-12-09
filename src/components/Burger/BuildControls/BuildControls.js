e5e1eecd4761a67661e0c002cb02f1ce07bb9c86
Steve Archuleta <stevearchuleta@me.com>
2019-12-09 11:31:48 -0800

    output multiple build controls; create a controls array of objects {label: and type: } in BuildControls.js; map each element into a BuildControl component in BuildControls.js; import BuildControls.js file into BurgerBuilder.js; return BuildControls component in BurgerBuilder component

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+   { ingredientLabel: 'Salad', type: 'salad' },
+   { ingredientLabel: 'Bacon', type: 'bacon' },
+   { ingredientLabel: 'Cheese', type: 'cheese' },
+   { ingredientLabel: 'Meat', type: 'meat' }
+];
 
 const buildControls = ( props ) => (
-   <div>
-      
+   <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+         <BuildControl key={ctrl.label} ingredientLabel={ctrl.ingredientLabel} />
+      ))}
    </div>
 );
 
