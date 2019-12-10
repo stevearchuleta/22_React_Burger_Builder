e99d1e65878c45d6ee8dd08136bf9d4b73977ae2
Steve Archuleta <stevearchuleta@me.com>
2019-12-10 10:18:45 -0800

    build logic for ingredientRemovedHandler in BurgerBuilder.js; pass event handler method reference to Build Controls in BurgerBuilder.js; use ingredientRemoved prop in the BuildControl component inside the BuildControls.js file; add onClick listener to the Less button in the individual buildControl functional component in BuildControl.js; disable/enable Less button using a for-in loop in BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,15 +41,38 @@ class BurgerBuilder extends Component {
    }
 
    removeIngreientHandler = (type) => {
-      
+      const oldCount = this.state.ingredients[type];
+      if(oldCount <= 0) {
+         return; //now nothing will happen if user tries to reduce an ingredient into negative territory (I do not want a negative array length error).
+      }
+      const updatedCount = oldCount - 1;
+      const updatedIngredients = {
+         ...this.state.ingredients
+      };
+      updatedIngredients[type] = updatedCount;
+      const priceDeduction = INGREDIENT_PRICES[type];
+      const oldPrice = this.state.totalPrice;
+      const newPrice = oldPrice - priceDeduction;
+      this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
    }
 
    render() {
+      const disabledInfo = {
+         ...this.state.ingredients //immutable clone of ingredients object
+      };
+      for (let key in disabledInfo) {
+         disabledInfo[key] = disabledInfo[key] <= 0 //loop through all of the keys in disabledInfo to check if any are less than or equal to zero. if so, a true value will disable the button
+      }
+      //form of my restructured object {salad: true, meat: false, etc...}
+      
       return (
          <Aux>
             <Burger ingredients={this.state.ingredients}/>
             <BuildControls 
-               ingredientAdded={this.addIngreientHandler}/>
+               ingredientAdded={this.addIngreientHandler}
+               ingredientRemoved={this.removeIngreientHandler}
+               disabled={disabledInfo}
+               />
          </Aux>
       );
    }
