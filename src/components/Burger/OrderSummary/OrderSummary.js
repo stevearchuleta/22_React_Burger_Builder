3e0ca6fc823a37c4236eeb72a275ead00c479d93
Steve Archuleta <stevearchuleta@me.com>
2019-12-23 10:19:32 -0800

    convert the functional orderSummary component into a class component; add the componentWillUpdate lifecycle hook to the OrderSummary component in order to discover when this component actually updates; convert functiional Modal component into a class component; add shouldComponentUpdate to Modal component in order to ensure that the orderSummary component only updates when the show property of the Modal component changes; add componentWillUpdate to the Modal component to confirm that the addition of shouldComonentUpdate to the Modal component actually prevents the calling of its child component, OrderSummary, until the ORDER NOW button is clicked and the modal is triggered...note: the wrapping component controls the update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+      //componentWillUpdate is no longer recommended by react, but this lifecycle hook was added as a testing/learning activity. Therefore, this particular component could be changed back into a functional component. 
+      componentWillUpdate() {
+         console.log('[OrderSummary] WillUpdate');
+      }
   
-      const ingredientSummary = Object.keys(props.ingredients)  //transofrm the expected ingredients from object format to an array
-         .map(igKey => {
-         return (
-            <li key={igKey}>
-               <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li> );
-         });
-      
+         render() {
+
+   const ingredientSummary = Object.keys(this.props.ingredients)  //transofrm the expected ingredients from an object to an array
+   .map(igKey => {
    return (
+      <li key={igKey}>
+         <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+      </li> );
+   });
+
+     return (
       <Aux>
-         <h3>Your Order</h3>
-         <p>A delicious burger with the following ingredients:</p>
-         <ul>
-            {ingredientSummary}
-         </ul>
-   <p><strong>TOTAL PRICE: ${props.price.toFixed(2)}</strong></p>
-         <p>Would you like to continue to the checkout page?</p>
-        <Button btnType="Danger" clicked={props.purchaseCancelled}>C A N C E L</Button>
-        <Button btnType="Success" clicked={props.purchaseContinued}>C O N T I N U E</Button>
-      </Aux>
-   )
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+         {ingredientSummary}
+      </ul>
+<p><strong>TOTAL PRICE: ${this.props.price.toFixed(2)}</strong></p>
+      <p>Would you like to continue to the checkout page?</p>
+     <Button btnType="Danger" clicked={this.props.purchaseCancelled}>C A N C E L</Button>
+     <Button btnType="Success" clicked={this.props.purchaseContinued}>C O N T I N U E</Button>
+   </Aux>
+     )
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;
