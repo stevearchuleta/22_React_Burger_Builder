cc5e18b090296d36c64b522adc56d70eff5be15f
Steve Archuleta <stevearchuleta@me.com>
2019-12-23 16:29:00 -0800

    change variable salad/Salad to lettuce/Lettuce in all occurances throughout the app (basically, Americanize my ingredient names)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
-         salad: 0.5,
+         lettuce: 0.5,
          bacon: 0.7,
          cheese: 0.4,
          meat: 1.3
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
    // }
    state = {
       ingredients: {
-         salad: 0,
+         lettuce: 0,
          bacon: 0,
          cheese: 0,
          meat: 0
