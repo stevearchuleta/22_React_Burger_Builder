95214743e87180be6ff42b807575cd2b9c26d7b1
Steve Archuleta <stevearchuleta@me.com>
2020-01-01 16:16:39 -0800

    create an axios post request in the purchaseContinueHandler within the BurgerBuilder container

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
          lettuce: 0.5,
@@ -40,8 +41,28 @@ class BurgerBuilder extends Component {
    }
 
    purchaseContinueHandler = () => {
-      alert("You Continue!")
-   }
+      // alert("You Continue!")
+      const order = {
+         ingredients: this.state.ingredients,
+         price: this.state.totalPrice,
+         customer: {
+            name: 'Steve Archuleta',
+            address: {
+               streetAddress: '6958 Treasure Way',
+               city: 'Sacramento',
+               state: 'California' 
+            },
+         email: 'stevearchuleta@me.com'
+         },
+         deliveryMethod: 'Uber Eats'
+
+      }
+      
+      axios.post('/orders.json', order)
+         .then(response => console.log(response))
+         .catch(error => console.log(error));
+
+         }
 
    updatePurchaseState (ingredients) {
       const sum = Object.keys(ingredients) //in order to sum up all of the ingreients, I first convert the ingredients object into an array of string entries ('salad', 'bacon', 'cheese', 'meat').
