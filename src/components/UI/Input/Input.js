8a0e5c89742ce11d26aa731bd80d5c679687a954
Steve Archuleta <stevearchuleta@me.com>
2020-01-16 15:52:19 -0800

    add a dropdowm component by adding a new select case in ContactData.js

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -17,6 +17,20 @@ const input = (props) => {
             {...props.elementConfig} //distribute props on textarea element, because I expect to receive the attributes that I set on the state in ContactData.js file as props for my textarea wrapper. In other words, I only need to set the elementType prop (in switch), then I pass any default HTML attributes from ContactData.js that I set on my Input component. 
             value={props.value}/>;
          break;
+         case ('select'):
+            inputElement = (
+            <select 
+               className={classes.ImputElement}
+               value={props.value}>
+              {props.elementConfig.options.map(option =>  (
+                 <option value={option.value}>
+                    {key.optionValue}
+                    {option.displayValue}
+                 </option>
+              ))};
+            </select>
+            );
+         break;
       default:
          inputElement = <input 
             className={classes.ImputElement}
