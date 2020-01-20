9dba9a0f20ebb35b8a9c61f1be5503f097b502cd
Steve Archuleta <stevearchuleta@me.com>
2020-01-20 14:49:10 -0800

    improve visual feedback; add to state a touched property to each element in the order form in contactData.js and initialize to false; update touched property inside the inputChangedHandler method to true; add touched property to the Input component inside <form> tag; add props.touched to the conditional if statement in input.js file

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,7 +4,7 @@ import classes from './Input.css';
 const input = (props) => {
    let inputElement = null;
    const inputClasses = [classes.ImputElement]
-   if (props.invalid && props.shouldValidate) {
+   if (props.invalid && props.shouldValidate && props.touched) {
       inputClasses.push(classes.Invalid);
    }
    switch ( props.elementType ) {
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                validation: {
                   required: true
                },
-               valid: false
+               valid: false,
+               touched: false
             },
             streetAddress: {
                elementType: 'input',
@@ -31,7 +32,8 @@ class ContactData extends Component {
                validation: {
                   required: true
                },
-               valid: false
+               valid: false,
+               touched: false
             },
             city: {
                elementType: 'input',
@@ -43,7 +45,8 @@ class ContactData extends Component {
                validation: {
                   required: true
                },
-               valid: false
+               valid: false,
+               touched: false
             },
             state: {
                elementType: 'input',
@@ -55,7 +58,8 @@ class ContactData extends Component {
                validation: {
                   required: true
                },
-               valid: false
+               valid: false,
+               touched: false
             },
             zipCode: {
                elementType: 'input',
@@ -69,7 +73,8 @@ class ContactData extends Component {
                   minLength: 5,
                   maxLength: 5
                },
-               valid: false
+               valid: false,
+               touched: false
             },
             email: {
                elementType: 'input',
@@ -81,7 +86,8 @@ class ContactData extends Component {
                validation: {
                   required: true
                },
-               valid: false
+               valid: false,
+               touched: false
             },
             deliveryMethod: {
                elementType: 'select',
@@ -147,6 +153,7 @@ class ContactData extends Component {
       };
       updatedFormElement.value = event.target.value;
       updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+      updatedFormElement.touched = true;
       updatedOrderForm[inputIdentifier] = updatedFormElement;
       console.log(updatedFormElement);
       this.setState({orderForm: updatedOrderForm});
@@ -170,6 +177,7 @@ class ContactData extends Component {
             value={formElement.config.value}
             invalid={!formElement.config.valid}
             shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler( event, formElement.id)}
             />
          ))};
