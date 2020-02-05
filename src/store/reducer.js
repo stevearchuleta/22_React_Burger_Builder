cf8737bfd961de7abf07af4a67b6f4d83c61b4b9
Steve Archuleta <stevearchuleta@me.com>
2020-02-05 08:54:45 -0800

    finish reducer for ingredients; add switch-case statement for actionTypes in reduce.js; temporarily comment out the componentDidMount code in BurgerBuilder.js; change ingredients value from null in reducer.js to a JS object with lettuce: 0 bacon:0 cheese:0 meat:0; in switch-case statement inside the reducer method, use the spread operator to make a copy of the current ...state then set ingredients to create a new object and override the given ingredients which is retreived as a payload of the reducer's action [action.ingredientName]: state.ingredients[action.ingredientName] + 1; do the exact opposite to reduce the ingredient count

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,36 @@
 import * actionTypes from './actions';
 
 const initialState = {
-   ingredients: null,
+   ingredients: {
+      lettuce: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+   },
    totalPrice: 4
 };
 
 const reducer = (state = initialState, action) => {
-
+   switch (action.type) {
+      case actionTypes.ADD_INGREDIENT:
+         return {
+            ...state,
+            ingredients: {
+               ...state.ingredients,
+               [action.ingredientName]: state.ingredients[action.ingredientName] + 1 //either lettuce, bacon, cheese, or meat will be added when receiving a new action
+            }
+         };
+      case actionTypes.REMOVE_INGREDIENT:
+         return {
+            ...state,
+            ingredients: {
+               ...state.ingredients,
+               [action.ingredientName]: state.ingredients[action.ingredientName] - 1 //either lettuce, bacon, cheese, or meat will be removed when receiving a new action
+            }
+         };
+      default:
+         return state;
+   }
 };
 
 export default reducer;
