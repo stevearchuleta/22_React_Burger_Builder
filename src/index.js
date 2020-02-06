25dffb3763b0c241b5ddf2ab95f86ed001a0f731
Steve Archuleta <stevearchuleta@me.com>
2020-02-06 13:35:34 -0800

    create action creators; import all actionTypes in burgerBuilder.js inside action folder; create addIngredient and removeIngredient action creators in burgerBuilder.js file inside actions folder; add an index.js file inside the action folder then export the addIngredient and removeIngredient from burgerBuilder.js in the index.js file inside the action folder; change actionTyps import name in the burgerBuilder.js file to burgerBuilderActions.js; change the argument in the dispatch method calls from a javaScript object to burgerBuilderActions.addIngredient(ingName) and burgerBuilderActions.removeIngredient(ingName)

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducers/burgerBuilder';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
    <Provider store={store}>
