d3f84d558e2c481083884ff9fa1d2dbafaac27d7
Steve Archuleta <stevearchuleta@me.com>
2020-02-06 12:36:23 -0800

    create an actions subfolder an a reducers subfolder inside the store folder; move the actions.js file into the actions folder; move the reducers.js file into the reducers folder; rename actions.js file to actionTypes.js then adjust import paths accordingly; add an order.js file in the actions folder, which will hold the action creators for submitting an order; create an order.js file inside the reducer folder; rename the reducer.js folder to burgerBuilder.js then adjust import paths accordingly; add a burgerBuilder.js file to the actions folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import reducer from './store/reducers/burgerBuilder';
 
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
