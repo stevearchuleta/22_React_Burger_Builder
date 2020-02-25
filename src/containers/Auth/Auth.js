5011d677c202ff0a2f3d38dbbc9ca5810b31f069
Steve Archuleta <stevearchuleta@me.com>
2020-02-25 09:45:05 -0800

    add actions #355; add AUTH actionTypes (start, success, fail, and asynchronous auth); create auth.js file in actions directory; export the auth method inside index.js within the actions directory ;connect actions to the auth container so that they can be dispatched (import everything as actions from ../../store/actions/index); add mapDispatchToProps to the Auth container; import {connect} from react-redux; execute connect in the export default by passing mapToDispatchProps into the connect method; add an onSubmit handler prop to the form in Auth.js inside the Auth container in order to execute onAuth; write an onSubmit method inside Auth.js within the Auth container

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index'
 
 class Auth extends Component {
    
@@ -68,6 +70,11 @@ class Auth extends Component {
       this.setState({controls: updatedControls});
    }
 
+   submitHandler = (event) => {
+      event.preventDefault(); //prevent page from reloading
+      this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+   }
+
    render() {
 
       const formElementsArray = []; //convert state object into an iterable array
@@ -94,7 +101,7 @@ class Auth extends Component {
             
             return (
                <div className={classes.Auth}>
-            <form>
+            <form onSubmit={this.submitHandler}>
                {form}
                <Button btnType="Success">SUBMIT</Button>
             </form>
@@ -103,4 +110,10 @@ class Auth extends Component {
    }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+   return {
+      onAuth: (email, password) => dispatch(actions.auth(email, password))
+   };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
