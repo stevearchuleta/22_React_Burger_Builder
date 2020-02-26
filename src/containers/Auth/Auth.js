48f788d89ea4a5fb04d166a1d2b266579160a32e
Steve Archuleta <stevearchuleta@me.com>
2020-02-26 12:25:06 -0800

    add a spinner #359; import spinner from /UI into auth.js file inside the Auth container; write mapStateToProps method in the Auth container - return loading state; connect mapStateToProps at export default in Auth container; render a <Spinner/> in my lieu of my form if this.state.loading; add error prop to mapStateToProps; display a Firebase INVALID_EMAIL message by creating a new const errorMessage with a conditional this.props.error that sets error message to this.props.error.message; access the Firebase INVALID_EMAIL error message on the dispatched authFail err object by accessing the original response.data.error in the auth.js file inside the actions directory

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,131 +1,155 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Input from '../../components/UI/Input/Input';
-import Button from '../../components/UI/Button/Button';
-import classes from './Auth.css';
-import * as actions from '../../store/actions/index'
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import classes from "./Auth.css";
+import * as actions from "../../store/actions/index";
 
 class Auth extends Component {
-   
-   state = {
-      controls: {
-         email: {
-            elementType: 'input',
-            elementConfig: {
-               type: 'email',
-               placeholder: 'Your Email Address'
-            },
-            value: '',
-            validation: {
-               required: true,
-               isEmail: true
-            },
-            valid: false,
-            touched: false
-         },
-         password: {
-            elementType: 'input',
-            elementConfig: {
-               type: 'password',
-               placeholder: 'Your Password'
-            },
-            value: '',
-            validation: {
-               required: true,
-               minLength: 6
-            },
-            valid: false,
-            touched: false
-         }
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Email Address"
+        },
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
       },
-      isSignUp: true
-   }
-
-   checkValidity( value, rules ) {
-      let isValid = true;
-      if ( rules.required ) {
-            isValid = value.trim() !== '' && isValid;
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Your Password"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
       }
-      if ( rules.minLength ) {
-            isValid = value.length >= rules.minLength && isValid;
-      } 
-      if ( rules.maxLength ) {
-         isValid = value.length <= rules.maxLength && isValid;
-   } 
-      return isValid;
-   }
-   
-   inputChangedHandler = (event, controlName) => {
-      const updatedControls = {
-         ...this.state.controls,
-         //only one of the properties in controls object will be overwrittien... controlName
-         [controlName]: {
-            ...this.state.controls[controlName],
-            //overwrite value; and overwrite valid with validation property rules
-            value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
-            touched: true
-         }
-      };
-      this.setState({controls: updatedControls});
-   }
+    },
+    isSignUp: true
+  };
 
-   submitHandler = (event) => {
-      event.preventDefault(); //prevent page from reloading
-      this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
-   }
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
 
-   switchAuthModeHandler = () => {
-      this.setState(prevState => {
-         return {isSignUp: !prevState.isSignUp};
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      //only one of the properties in controls object will be overwrittien... controlName
+      [controlName]: {
+        ...this.state.controls[controlName],
+        //overwrite value; and overwrite valid with validation property rules
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true
+      }
+    };
+    this.setState({ controls: updatedControls });
+  };
+
+  submitHandler = event => {
+    event.preventDefault(); //prevent page from reloading
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignUp
+    );
+  };
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return { isSignUp: !prevState.isSignUp };
+    });
+  };
+
+  render() {
+    const formElementsArray = []; //convert state object into an iterable array
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
       });
-   }
+    }
 
-   render() {
+    //loop through array to create a dynamically generated input form
+    let form = formElementsArray.map(formElement => (
+      <Input
+        key={formElementsArray.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={event => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
 
-      const formElementsArray = []; //convert state object into an iterable array
-      for ( let key in this.state.controls ) {
-         formElementsArray.push({
-            id: key,
-            config: this.state.controls[key]
-         });
-      }
-      
-      //loop through array to create a dynamically generated input form
-      const form = formElementsArray.map(formElement => (
-         <Input
-            key={formElementsArray.id}
-            elementType={formElement.config.elementType}
-            elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value}
-            invalid={!formElement.config.valid}
-            shouldValidate={formElement.config.validation}
-            touched={formElement.config.touched}
-            changed={(event) => this.inputChangedHandler( event, formElement.id)}
-            />
-            )); 
-            
-            return (
-               <div className={classes.Auth}>
-            <form onSubmit={this.submitHandler}>
-               {form}
-               <Button btnType="Success">SUBMIT</Button>
-            </form>
-            <Button 
-               clicked={this.switchAuthModeHandler}
-               btnType="Danger">CLICK TOGGLE TO {this.state.isSignUp ? 'SIGNIN MODE' : 'SIGNUP MODE'}
-            </Button>
-         </div>
-      );
-   }
+    if (this.props.loading) {
+      form = <Spinner />;
+    }
+
+    let errorMessage = null;
+    if (this.props.error) {
+      errorMessage = <p>{this.props.error.message}</p>;
+    }
+
+    return (
+      <div className={classes.Auth}>
+        {errorMessage}
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
+        <Button clicked={this.switchAuthModeHandler} btnType="Danger">
+          CLICK TOGGLE TO {this.state.isSignUp ? "SIGNIN MODE" : "SIGNUP MODE"}
+        </Button>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-   //dispatch the auth action creator with three (3) arguments
-   return {
-      onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
-   };
+//obtain the loading piece of my state and map a slice of it to my local props
+const mapStatetoProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error
+  };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+const mapDispatchToProps = dispatch => {
+  //dispatch the auth action creator with three (3) arguments
+  return {
+    onAuth: (email, password, isSignUp) =>
+      dispatch(actions.auth(email, password, isSignUp))
+  };
+};
+
+export default connect(mapStatetoProps, mapDispatchToProps)(Auth);
