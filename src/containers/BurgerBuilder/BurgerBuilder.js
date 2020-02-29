78342f30d524851b826d5ded1ea234bff663aec9
Steve Archuleta <stevearchuleta@me.com>
2020-02-29 14:37:40 -0800

    forward unauthenticated users #364; add a conditional props.isAuthenticated statement to the NavigationItems link in the NavigationItems.js file; retreive the isAuthenticated information insidethe mapStateToProps in the Auth.js container by comparing state.auth.token to not null; import {Redirect} component from react-router-dom; add an authRedirect conditional if statement that checks for this.props.isAuthenticated and the page is then redirected to '/' in Auth.js; return authRedirect in Auth.js; add isAuth prop into mapStateToProps in the BurgerBuilder control and compare state.auth.token to not equal to null; pass isAuthenticated to the BuildControls component; receive isAuth prop in the Button onClick event in the BuildControls.js file inside BuildControls directory which is in the Burger component; add a conditional if statement in the BurgerBuilder container that checks this.props.isAuthenticated and conditionally sets purchasing state to true or else it uses the this.props.history to push the user to the '/auth' page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,11 @@ class BurgerBuilder extends Component {
    }
 
    purchaseHandler = () => {
-      this.setState({purchasing: true});
+      if (this.props.isAuthenticated) {
+         this.setState({purchasing: true});
+      } else {
+         this.props.history.push('/auth');
+      }
    }
 
    purchaseCancelHandler = () => {
@@ -84,6 +88,7 @@ class BurgerBuilder extends Component {
                disabled={disabledInfo}
                purchasable={this.updatePurchaseState(this.props.ings)}
                ordered={this.purchaseHandler}
+               isAuth={this.props.isAuthenticated}
                price={this.props.price} />
             
             </Aux>
@@ -112,7 +117,8 @@ const mapStateToProps = state => {
    return {
       ings: state.burgerBuilder.ingredients,
       price: state.burgerBuilder.totalPrice,
-      error: state.burgerBuilder.error
+      error: state.burgerBuilder.error,
+      isAuthenticated: state.auth.token !== null
    };
 }
 
